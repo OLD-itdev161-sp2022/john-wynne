b9fee65e6b51c104ef3bc932e0ffd62e243ca266
john wynne <wynnej1@gamatc.matc.edu>
2022-02-10 20:19:11 -0600

    completed Activity 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 import connectDatabase from './config/db';
-
+import {check, validationResult} from 'express-validator';
 
 //initialize express application//
 const app = express();
@@ -26,10 +26,24 @@ res.send('http get request sent to root api endpoint')
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users',(req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post(
+    '/api/users',
+    [
+check ( 'name', 'please enter your name').not().isEmpty(),
+check ( 'email', 'Please enter a valid email').isEmail(),
+check ('password', 'Please enter a password with 6 or more characters').isLength({ min:6 })
+
+
+    ],
+    (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array ()});
+    } else {
+     return res.send(req.body);
+    }
+}
+);
 
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
