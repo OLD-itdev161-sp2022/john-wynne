c7735796f82acf8279f8e99728c95d6ff9ea3db1
john wynne <wynnej1@gamatc.matc.edu>
2022-02-17 17:00:22 -0600

    completed activity 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  state = {
+    data: null
+  }
+componentDidMount(){
+  axios.get(' http://localhost:5000')
+  .then ((response) => {
+this.setState ({
+  data: response.data
+})
+  })
+  .catch ((error) => {
+    console.error(`Error fetching data: ${error}`);
+  });
+}
+
+  render (){
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       GoodThings
       </header>
+      {this.state.data}
     </div>
   );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
-
+import { check, validationResult} from 'express validator';
+import cors from 'cors';
 
 //initialize express application//
 const app = express();
@@ -8,10 +9,19 @@ const app = express();
 //connect database
 connectDatabase ();
 
+//Middleware
+app.arguments(express.json({extended: false}));
+app.arguments(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
+
 //API endpoints//
 app.get('/', (req,res) =>
 res.send('http get request sent to root api endpoint')
 );
 
 //connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
